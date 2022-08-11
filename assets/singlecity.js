fb52d75adadcf3a8674fe33230ead5b1f29c291a
MertCetin <mert.cetnn@gmail.com>
2022-08-11 17:42:47 -0400

    trying to debug localstorage leng

--- assets/singlecity.js
+++ assets/singlecity.js
@@ -1,80 +1,52 @@
-var cityNameBtn=document.getElementsByClassName('cityNameBtn')
-var input =document.getElementById('domTextElement');
-const btn =document.getElementById('searchBtn').addEventListener("click",btnF)
+var cityNameBtn = document.getElementsByClassName("cityNameBtn");
 
- // each click takes value and store in local storage
-function btnF(){
-localStorage.setItem('City',input.value )}
-// can not make it more than 1 history of search
+const btn = document
+  .getElementById("searchBtn")
+  .addEventListener("click", addItem);
 
-
-
-
-//takes setted local storage value put in history
-
-var SavedCity=localStorage.getItem("City")
-if(SavedCity==null){
-  var num=0
-}else {var num =SavedCity.length}
-
-
-
-
-//creating <li>
-var  NewListItem =function(newLi){
-var li =document.createElement("li")
-li.className="CityNames"
-var liBtn=document.createElement("button")
-liBtn.className="cityNameBtn"
-liBtn.type="button"
-var liSpan =document.createElement("span")
-liSpan.className="storageNamesLi"
-
-liBtn.appendChild(liSpan)
-li.appendChild(liBtn)
-aside.div.ul.appendChild(li)
-
-
+// each click takes value and store in local storage
+function addItem() {
+  var input = document.getElementById("domTextElement");
+  var newCity=localStorage.setItem("addItem", input.value);
+  return;
 }
 
+//takes setted local storage value put in history
+var getData = (document.getElementById("domTextElement").value =
+  localStorage.getItem("addItem"));
+if (getData == null) {
+  var num = 0;
+} else {
+  var num = getData.length;
+ 
+}
+let x = localStorage.length;
+for (i = 0; i < localStorage.length; i++) {
+  x = localStorage.key(i);}
 
+addItem();
 
-NewListItem=SavedCity
+//creating <li>
+var NewListItem = function (newLi) {
+  var li = document.createElement("li");
+  li.className = "CityNames";
+  var liBtn = document.createElement("button");
+  liBtn.className = "cityNameBtn";
+  liBtn.type = "button";
+  var liSpan = document.createElement("span");
+  liSpan.className = "storageNamesLi";
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
-
-
-
+  liBtn.appendChild(liSpan);
+  li.appendChild(liBtn);
+  aside.div.ul.appendChild(li);
+};
+if (addItem == true) {
+  let NewListItem;
+}
 
 //fetching API
-fetch('http://api.weatherapi.com/v1/forecast.json?key=b622ade93acc444b88c30856220608&q=Albany&days=1&aqi=no&alerts=no')
+fetch(
+  "http://api.weatherapi.com/v1/forecast.json?key=b622ade93acc444b88c30856220608&q=Albany&days=1&aqi=no&alerts=no"
+)
   .then((response) => response.json())
   .then((data) => console.log(data));
-
-
-
-
-
-
-
-
-
-
-
-
-    
