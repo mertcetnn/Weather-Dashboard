c70d7a2dc327202e8944248728459116002c5a1c
MertCetin <mert.cetnn@gmail.com>
2022-08-13 22:13:17 -0400

    search matching source and routing to array

--- assets/singlecity.js
+++ assets/singlecity.js
@@ -1,69 +1,75 @@
 var cityNameBtn = document.getElementsByClassName("cityNameBtn");
+var aside =document.getElementsByClassName("sideBar")
+var list =document.getElementsByClassName("sideBarUl")
+var input = document.getElementById("domTextElement");
+var searchHistory=[]
 
-var valueOfInpt =0;
 
 //adding event listener for "searchBtn"
-const btnStore = document.getElementById("searchBtn")
-.addEventListener("click", addItem);
+const btnStore = document.getElementById("searchBtn").addEventListener("click", addItem);
 
 
 // each click takes value and store in local storage
 function addItem() {
-  var input = document.getElementById("domTextElement");
- valueOfInpt++;
-localStorage.setItem(input.value,"addItem");
-                    //(value,key)
-  return ;
+  pickCity(input.value);
+  
+searchHistory.push(input.value)
+
+console.log(searchHistory)
+
+localStorage.setItem("pastCity",searchHistory);
+
+return input;   //takes setted local storage value put in history
 }
-if (localStorage ==null)
-  {value.input=0}
-  else{length=valueOfInpt}
-
-
-  //takes setted local storage value put in history
-var getData = (document.getElementById("domTextElement").value =
-  localStorage.getItem("addItem"));
-
-
- 
- 
-//   function clearLocalStorage(){
-//     localStorage.removeItem("additem")
-//     return localStorage= null;
-// }
-// clearLocalStorage();
+if(input== null){indow.localStorage.removeItem("addItem")}
 
 
 
+if (searchHistory.length){
+  function createElement(){
+    for(i=0 ; i<searchHistory.length;i++){
+        
+      var li = document.createElement("li")
+        li.className = "CityNames"
+        var liBtn = document.createElement("button")
+        liBtn.className = "cityNameBtn"
+        liBtn.type = "button"
+        var liSpan = document.createElement("span")
+        liSpan.className = "storageNamesLi"
+      
+        liBtn.appendChild(liSpan);
+        li.appendChild(liBtn);
+       aside.list.appendChild(li);
+       
 
+      
 
-
-
-
-
-
-
-//creating <li>
-var NewListItem = function (newLi) {
-  var li = document.createElement("li");
-  li.className = "CityNames";
-  var liBtn = document.createElement("button");
-  liBtn.className = "cityNameBtn";
-  liBtn.type = "button";
-  var liSpan = document.createElement("span");
-  liSpan.className = "storageNamesLi";
-
-  liBtn.appendChild(liSpan);
-  li.appendChild(liBtn);
-  aside.div.ul.appendChild(li);
-};
-if (addItem == true) {
-  let NewListItem;
+  }}
 }
 
+
+
+function pickCity(singleCity){
 //fetching API
 fetch(
-  "http://api.weatherapi.com/v1/forecast.json?key=b622ade93acc444b88c30856220608&q=Albany&days=1&aqi=no&alerts=no"
+  "http://api.weatherapi.com/v1/forecast.json?key=b622ade93acc444b88c30856220608&q="+`${singleCity}`+"&days=1&aqi=no&alerts=no"
+
 )
   .then((response) => response.json())
-  .then((data) => console.log(data));
+  .then((data) => {
+    console.log(data)
+
+  if ( data.error){
+    alert("No matching location found.")
+  }
+  });
+
+return  
+  
+
+
+}
+
+// if (response !== input.value){
+//   alert("No matching location found.")
+// }
