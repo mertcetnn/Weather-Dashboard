ea1b61ddb9280f43c1695020a88bfc94badbffaf
MertCetin <mert.cetnn@gmail.com>
2022-08-15 18:57:44 -0400

    addeventListener Add

---
+++ assets/allsearch.js
@@ -0,0 +1,18 @@
+
+
+
+searchBtn.addEventListener("click",() => {
+
+    var newBtn=document.createElement('button')
+    newBtn.classList.add("cityNameBtn")
+    newBtn.textContent = input.value;
+     
+    divCity.appendChild(newBtn)
+  
+  
+  }
+  )
+   
+
+
+
--- assets/singlecity.js
+++ assets/singlecity.js
@@ -1,75 +1,112 @@
-var cityNameBtn = document.getElementsByClassName("cityNameBtn");
-var aside =document.getElementsByClassName("sideBar")
-var list =document.getElementsByClassName("sideBarUl")
+
+var aside = document.getElementsByClassName("sideBar");
 var input = document.getElementById("domTextElement");
-var searchHistory=[]
+var searchHistory = [];
+var divCity = document.getElementById("storageNames")
+var ul = document.getElementById("storageNamesUl");
+var li = document.getElementsByClassName("cityNames");
+var cityBtn = document.getElementsByClassName("cityNameBtn");
+var citySpan = document.getElementsByClassName("storageNamesLi");
 
 
 //adding event listener for "searchBtn"
-const btnStore = document.getElementById("searchBtn").addEventListener("click", addItem);
-
+const btnStore = document
+  .getElementById("searchBtn")
+  .addEventListener("click", addItem);
 
 // each click takes value and store in local storage
 function addItem() {
   pickCity(input.value);
-  
-searchHistory.push(input.value)
 
-console.log(searchHistory)
+  searchHistory.push(input.value);
 
-localStorage.setItem("pastCity",searchHistory);
+  console.log(searchHistory);
 
-return input;   //takes setted local storage value put in history
+  localStorage.setItem("pastCity", searchHistory);
+
+  return input; //takes setted local storage value put in history
 }
-if(input== null){indow.localStorage.removeItem("addItem")}
-
-
-
-if (searchHistory.length){
-  function createElement(){
-    for(i=0 ; i<searchHistory.length;i++){
-        
-      var li = document.createElement("li")
-        li.className = "CityNames"
-        var liBtn = document.createElement("button")
-        liBtn.className = "cityNameBtn"
-        liBtn.type = "button"
-        var liSpan = document.createElement("span")
-        liSpan.className = "storageNamesLi"
-      
-        liBtn.appendChild(liSpan);
-        li.appendChild(liBtn);
-       aside.list.appendChild(li);
-       
-
-      
-
-  }}
+if (input == null) {
+  window.localStorage.removeItem("pastCity");
 }
 
 
+// //creating element for past
+//                                 //>>>>>>>>>>>>>>>>>Doesnt work
+// if (searchHistory.length  > 0 || searchHistory.length == 0) {
+//     function createElement() {
+//       for (i = 0; i < searchHistory.length; i++) {
+//   var storedCities = JSON.parse(localStorage.getItem("pastCity")) ;
+//   var pastSearchesEl = document.getElementById('cityNamebtn');
 
-function pickCity(singleCity){
-//fetching API
-fetch(
-  "http://api.weatherapi.com/v1/forecast.json?key=b622ade93acc444b88c30856220608&q="+`${singleCity}`+"&days=1&aqi=no&alerts=no"
 
-)
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data)
+// console.log(storedCities)
+// console.log(pastSearchesEl)
 
-  if ( data.error){
-    alert("No matching location found.")
-  }
-  });
 
-return  
+// pastSearchesEl.innerHTML ='';
+// var pastCityBtn = document.createElement("button");
+//       pastCityBtn.classList.add("cityNameBtn");
+//       pastCityBtn.textContent = `${storedCities[i].city}`;
+//       pastSearchesEl.appendChild(pastCityBtn);
+//   }
   
 
+//     }}
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var currentCity;
+
+
+
+function pickCity(singleCity) {
+  //fetching API
+  fetch(
+    "http://api.weatherapi.com/v1/forecast.json?key=b622ade93acc444b88c30856220608&q=" +
+      `${singleCity}` +
+      "&days=1&aqi=no&alerts=no"
+  )
+    .then((response) => response.json())
+    .then(function(data) {
+
+
+// current weather
+var currentConditionsEl = $('#dataMainDivSpan');
+
+
+// create city name element and display
+var cityNameEl = $('<h2>');
+cityNameEl.text(currentCity);
+currentConditionsEl.append(cityNameEl);
+
+
+
+
+
+
+
+
+      
+      console.log(data);
+
+      if (data.error) {
+        alert("No matching location found.");
+      }
+    });
+
+  return;
 }
-
-// if (response !== input.value){
-//   alert("No matching location found.")
-// }
