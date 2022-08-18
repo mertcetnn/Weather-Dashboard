30ed47f30d6d0735831ea4ac26fda3198acb947e
MertCetin <mert.cetnn@gmail.com>
2022-08-18 17:08:54 -0400

    main div pulling api and creating is done  but img src need work on it

--- assets/allsearch.js
+++ assets/allsearch.js
@@ -1,17 +1,7 @@
 
 
 
-searchBtn.addEventListener("click",() => {
 
-    var newBtn=document.createElement('button')
-    newBtn.classList.add("cityNameBtn")
-    newBtn.textContent = input.value;
-     
-    divCity.appendChild(newBtn)
-  
-  
-  }
-  )
    
 
 
--- assets/singlecity.js
+++ assets/singlecity.js
@@ -1,13 +1,11 @@
-
 var aside = document.getElementsByClassName("sideBar");
 var input = document.getElementById("domTextElement");
 var searchHistory = [];
-var divCity = document.getElementById("storageNames")
+var divCity = document.getElementById("storageNames");
 var ul = document.getElementById("storageNamesUl");
 var li = document.getElementsByClassName("cityNames");
 var cityBtn = document.getElementsByClassName("cityNameBtn");
 var citySpan = document.getElementsByClassName("storageNamesLi");
-
 
 //adding event listener for "searchBtn"
 const btnStore = document
@@ -18,12 +16,6 @@ const btnStore = document
 function addItem() {
   pickCity(input.value);
 
-  searchHistory.push(input.value);
-
-  console.log(searchHistory);
-
-  localStorage.setItem("pastCity", searchHistory);
-
   return input; //takes setted local storage value put in history
 }
 if (input == null) {
@@ -31,47 +23,8 @@ if (input == null) {
 }
 
 
-// //creating element for past
-//                                 //>>>>>>>>>>>>>>>>>Doesnt work
-// if (searchHistory.length  > 0 || searchHistory.length == 0) {
-//     function createElement() {
-//       for (i = 0; i < searchHistory.length; i++) {
-//   var storedCities = JSON.parse(localStorage.getItem("pastCity")) ;
-//   var pastSearchesEl = document.getElementById('cityNamebtn');
-
-
-// console.log(storedCities)
-// console.log(pastSearchesEl)
-
-
-// pastSearchesEl.innerHTML ='';
-// var pastCityBtn = document.createElement("button");
-//       pastCityBtn.classList.add("cityNameBtn");
-//       pastCityBtn.textContent = `${storedCities[i].city}`;
-//       pastSearchesEl.appendChild(pastCityBtn);
-//   }
-  
-
-//     }}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 var currentCity;
-
-
 
 function pickCity(singleCity) {
   //fetching API
@@ -81,27 +34,65 @@ function pickCity(singleCity) {
       "&days=1&aqi=no&alerts=no"
   )
     .then((response) => response.json())
-    .then(function(data) {
-
-
-// current weather
-var currentConditionsEl = $('#dataMainDivSpan');
-
-
-// create city name element and display
-var cityNameEl = $('<h2>');
-cityNameEl.text(currentCity);
-currentConditionsEl.append(cityNameEl);
-
-
-
-
-
-
-
-
-      
+     
+    .then(function (data) {
       console.log(data);
+      var lat=data.location.lat
+      var lon=data.location.lon
+      var uvFirstData=data.current.uv
+      searchHistory.push(data.location.name);
+
+      console.log(searchHistory);
+
+
+      localStorage.setItem("pastCity", JSON.stringify(searchHistory)); //search that
+
+      for (let i=0 ; i <searchHistory.length ; i++){
+        console.log(searchHistory[i]) 
+        if (searchHistory[i] == data.location.name){
+
+          cityBtn = document.createElement("button");
+          cityBtn.classList.add("cityNameBtn");
+          cityBtn.textContent = data.location.name;
+          divCity.appendChild(cityBtn);
+        }
+        
+      }
+      
+      var API2="3604a2a806490268bfd78ce7aba0d8e9"
+      var latNumber=lat
+      var lonNumber=lon
+     fetch("https://api.openweathermap.org/data/2.5/weather?lat="+latNumber+"&lon="+lonNumber+"&appid="+API2+"&units=imperial")
+     .then ((response)=>response.json())
+     .then (function(data){
+      console.log(data)
+         
+      var mainDivH1=document.getElementById('dataMainDivSpan')
+      mainDivH1.textContent=data.name
+      var iconImg="http://openweathermap.org/img/wn/04n"+iconName+"@2x.png"
+      var iconName=data.weather.icon
+      var icon=document.getElementById('icon')
+      icon.setAttribute('src',iconImg)
+      
+
+      var choosenLiTemp=document.getElementById('choosenTemp')
+      choosenLiTemp.textContent=data.main.temp
+      var choosenWind=document.getElementById('choosenWind')
+      choosenWind.textContent=data.wind.deg
+      var choosenHumidty=document.getElementById('choosenHumidty')
+      choosenHumidty.textContent=data.main.humidity
+      var choosenUV=document.getElementById('uvIndex')
+      choosenUV.textContent=uvFirstData
+
+
+
+
+
+
+
+     })
+
+
 
       if (data.error) {
         alert("No matching location found.");
