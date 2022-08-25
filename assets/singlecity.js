bc78a78523c03a051470dc43184f126f8917d570
MertCetin <mert.cetnn@gmail.com>
2022-08-24 23:31:51 -0400

    appendchild and img problem solved

--- assets/singlecity.js
+++ assets/singlecity.js
@@ -34,9 +34,9 @@ function pickCity(singleCity) {
       "&days=1&aqi=no&alerts=no"
   )
     .then((response) => response.json())
-     
-    .then(function (data) {
-      console.log(data);
+     //converting lat and log data to vareible
+    .then(function (data) { 
+      console.log(data); 
       var lat=data.location.lat
       var lon=data.location.lon
       var uvFirstData=data.current.uv
@@ -45,7 +45,9 @@ function pickCity(singleCity) {
       console.log(searchHistory);
 
 
-      localStorage.setItem("pastCity", JSON.stringify(searchHistory)); //search that
+      //setting json.stringfy data to local storage
+
+      localStorage.setItem("pastCity", JSON.stringify(searchHistory));
 
       for (let i=0 ; i <searchHistory.length ; i++){
         console.log(searchHistory[i]) 
@@ -58,7 +60,7 @@ function pickCity(singleCity) {
         }
         
       }
-      
+      //using second api  fetching and taking data from inside
       var API2="3604a2a806490268bfd78ce7aba0d8e9"
       var latNumber=lat
       var lonNumber=lon
@@ -66,7 +68,7 @@ function pickCity(singleCity) {
      .then ((response)=>response.json())
      .then (function(data){
       console.log(data)
-         //this date code for matching date and dxata date
+         //this date code for matching date and data date
       const date = new Date();
       
 
@@ -87,13 +89,15 @@ function pickCity(singleCity) {
 
 
 
-      "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
       
-      var iconImg="http://http://openweathermap.org/img/wn/"+ iconName +".png"
+      
+     
     
-       var weather=data.list[1].weather[0]
-      var iconName=data.list[1].weather[0].icon
-
+  
+      var iconName=data.list[0].weather[0].icon
+      var iconImg="https://openweathermap.org/img/wn/"+iconName+"@2x.png"
+console.log(iconName)
+console.log(iconImg)
 
       var icon=document.getElementById('icon')
       icon.setAttribute('src',iconName)
@@ -113,6 +117,60 @@ function pickCity(singleCity) {
       choosenUV.textContent=uvFirstData
 
 
+//appenChild variebles section
+
+for (i=0; i<5 ;i++ <4){
+
+const mainDiv=document.createElement('div')
+mainDiv.setAttribute("id","week-forecast-data")
+const mainP =document.createElement('p')
+mainP.setAttribute('class','week-forecast-p')
+const h5=document.createElement('h5')
+h5.setAttribute("id","week-forecast-date")
+
+const secondP=document.createElement('p')
+secondP.setAttribute('class','week-forecast-p')
+const mainImg=document.createElement('img')
+mainImg.setAttribute('id','week-forecast-img')
+mainImg.setAttribute('src',iconImg)
+
+const thirdP=document.createElement('p')
+thirdP.setAttribute('class','week-forecast-p')
+const tempDiv=document.createElement("div")
+tempDiv.setAttribute('id','week-forecast-temp')
+
+const forthP=document.createElement('p')
+forthP.setAttribute('class','week-forecast-p')
+const windDiv=document.createElement('div')
+windDiv.setAttribute('id','week-forecast-wind')
+
+const fifthP=document.createElement('p')
+fifthP.setAttribute('class','week-forecast-p')
+const humDiv=document.createElement('div')
+humDiv.setAttribute('id','week-forecast-humidty')
+
+
+
+// append child sectoin
+const weekDayForecast=document.getElementById('week-forecast')
+weekDayForecast.appendChild(mainDiv)
+mainDiv.appendChild.mainP
+
+
+mainP.appendChild(h5)
+
+mainDiv.appendChild(secondP)
+secondP.appendChild(mainImg)
+
+mainDiv.appendChild(thirdP)
+ thirdP.appendChild(tempDiv)
+
+ mainDiv.appendChild(forthP)
+ forthP.appendChild(windDiv)
+
+  mainDiv.appendChild(fifthP)
+fifthP.appendChild(humDiv)
+}
 
 
      
@@ -130,64 +188,6 @@ function pickCity(singleCity) {
   
   return;
 }
-//appenChild variebles section
-
-
-  
-
-   const mainDiv=document.createElement('div')
-   mainDiv.setAttribute("id","week-forecast")
-   const mainLi =document.createElement('p')
-   mainLi.setAttribute('class','week-forecast-p')
-   const h5=document.createElement('h5')
-   h5.setAttribute("id","week-forecast-date")
-
-   const secondLi=document.createElement('p')
-   secondLi.setAttribute('class','week-forecast-p')
-   const mainImg=document.createElement('img')
-   mainImg.setAttribute('id','week-forecast-img')
-   mainImg.setAttribute('src',iconImg)
-
-   const thirdLi=document.createElement('p')
-   thirdLi.setAttribute('class','week-forecast-p')
-   const tempDiv=document.createElement("div")
-   tempDiv.setAttribute('id','week-forecast-temp')
-
-   const forthLi=document.createElement('p')
-   forthLi.setAttribute('class','week-forecast-p')
-   const windDiv=document.createElement('div')
-   windDiv.setAttribute('id','week-forecast-wind')
-
-   const fifthLi=document.createElement('p')
-   fifthLi.setAttribute('class','week-forecast-p')
-   const humDiv=document.createElement('div')
-   humDiv.setAttribute('id','week-forecast-humidty')
-
-
-
-// append child sectoin
-
-  weekDayForecast.appendChild(mainDiv)
-   mainDiv.appendChild,mainLi
-
-  
-   mainLi.appendChild(h5)
-
-   mainUl.appendChild(secondLi)
-   secondLi.appendChild(mainImg)
-
-    mainUl.appendChild(thirdLi)
-    thirdLi.appendChild(tempDiv)
-
-    mainUl.appendChild(forthLi)
-    forthLi.appendChild(windDiv)
-
-     mainUl.appendChild(fifthLi)
-   fifthLi.appendChild(humDiv)
-   var weekDayForecast=document.getElementById('week-forecast')
 
    
 
-
-   
-  
